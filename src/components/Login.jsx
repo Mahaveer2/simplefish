0266b6e55f13bec64b1cf7cd205021f133048757
Partab Rai <amrani.partab@gmail.com>
2022-12-02 09:35:13 +0500

    work power and energy

---
+++ src/components/Login.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react'
+import { supabase } from '../supaBaseClient';
+
+function Login() {
+  const [email,setEmail] =useState("");
+  const [pass,setPass] =useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase
+    .from('user')
+    .insert([
+      {email: email, password: pass },
+    ])
+    setEmail("");
+    setPass("");
+
+    alert("error occured in the server. Please try again later.");
+  }
+  return (
+    <section name="Login" className='dark'>
+      <section className=" h-[100vh] dark:bg-gray-900">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <a
+      href="#"
+      className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+    >
+      <img
+        className="w-8 h-8 mr-2"
+        src="https://thumbs.dreamstime.com/b/vector-simple-logo-template-physics-label-line-style-logotype-easy-to-use-abstract-sign-emblem-90995002.jpg"
+        alt="logo"
+      />
+      Physics Master
+    </a>
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Sign in to your account via Facebook
+        </h1>
+        <form onSubmit={(e) => handleSubmit(e)} className="space-y-4 md:space-y-6" action="#">
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-zinc-600 focus:border-zinc-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@company.com"
+              required=""
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={pass}
+              onChange={e => setPass(e.target.value)}
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-zinc-600 focus:border-zinc-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required=""
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="remember"
+                  aria-describedby="remember"
+                  type="checkbox"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-zinc-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-zinc-600 dark:ring-offset-gray-800"
+                  required=""
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label
+                  htmlFor="remember"
+                  className="text-gray-500 dark:text-gray-300"
+                >
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <a
+              href="#"
+              className="text-sm font-medium text-zinc-600 hover:underline dark:text-zinc-500"
+            >
+              Forgot password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="w-full text-white bg-zinc-600 hover:bg-zinc-700 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-zinc-600 dark:hover:bg-zinc-700 dark:focus:ring-zinc-800"
+          >
+            Sign in with facebook
+          </button>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            Don’t have an account yet?{" "}
+            <a
+              href="#"
+              className="font-medium text-zinc-600 hover:underline dark:text-zinc-500"
+            >
+              Sign up
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+    </section>
+  )
+}
+
+export default Login
